049f8d16c7aa70b70d91f397cf419a95c0b33079
morthon <martin.lecochon@laposte.net>
2018-02-02 19:51:44 +0100

    Added a separate help.txt file. The interpreter now manages trailing whitespaces after :commands

--- main.hs
+++ main.hs
@@ -2,8 +2,6 @@
 
 import System.IO
 import Data.Char
-import System.Process
-import GHC.IO.Exception
 
 -- DATA DEFINITIONS
 
@@ -32,7 +30,7 @@ data Error = IllegalChar Char
 instance Show Error where
     show e = "Error: " ++ case e of
                            ParseError    -> "no parse"
-                           IllegalChar c -> "illegal use of character '" 
+                           IllegalChar c -> "illegal use of character '"
                                             ++ (c:"'")
 
 -- EVALUATOR
@@ -68,7 +66,7 @@ replaceByIn v e (Lambda w e1)       = if (Singleton w) `isIn` e
                                       then doit v e $ Lambda (rename w)
                                               (replaceByIn w 
                                                            (Singleton 
-                                                             $ rename w) 
+                                                             $ rename w)
                                                            e1)
                                       else doit v e (Lambda w e1)
         where doit v e (Lambda w e1)  = if v == w 
@@ -82,7 +80,7 @@ replaceByIn v e (Lambda w e1)       = if (Singleton w) `isIn` e
 
 isIn :: Expr -> Expr -> Bool
 isIn (Singleton v)         (Singleton w)       = (v == w)
-isIn v@(Singleton _)       (Application e1 e2) = v `isIn` e1 || v `isIn` e2 
+isIn v@(Singleton _)       (Application e1 e2) = v `isIn` e1 || v `isIn` e2
 isIn v@(Singleton c)       (Lambda c2 e)       = c == c2 || isIn v e
 isIn (Application e1 e2)   e3                  = e1 `isIn` e3 || e2 `isIn` e3
 isIn (Lambda v1 e1)        e2                  = (Singleton v1) `isIn` e2
@@ -92,25 +90,21 @@ isIn (Lambda v1 e1)        e2                  = (Singleton v1) `isIn` e2
 -- PARSER
 
 forbidden :: Char -> Bool
-forbidden v = v == ' ' 
-            || v == '_' 
-            || v == '(' 
-            || v == ')'
-            || v == '\\'
-            || isNumber v
+forbidden v = v `elem` "_ ()\\" || isNumber v
 
 parseLambda :: String -> Either Error Expr
 parseLambda [v]                = if forbidden v
-                                 then Left  $ IllegalChar v 
+                                 then Left  $ IllegalChar v
                                  else Right $ Singleton (v, 0)
 parseLambda ('(':v:")")        = parseLambda [v]
 parseLambda ('(':'\\':v:'.':e) = if forbidden v 
                                  then Left $ IllegalChar v
                                  else Lambda (v, 0) <$> (parseLambda $ addParens
                                                                    $ init e)
+-- Multiple parameters lambda expression
 parseLambda ('(':'\\':v:e)     = if forbidden v
                                  then Left $ IllegalChar v
-                                 else Lambda (v, 0) <$> (parseLambda $ "(\\" 
+                                 else Lambda (v, 0) <$> (parseLambda $ "(\\"
                                                                      ++ e)
 parseLambda ('(':s)            = Application <$> (fst splitted)
                                              <*> (snd splitted)
@@ -141,22 +135,19 @@ addParens s = if head s == '\\' || checkApps 0 s
 
 -- TODO :
 --  * Enable combinator definitions and reading them from a file.
---  ! Ensure that there are no infinite loops or evaluation errors 
+--  ! Ensure that there are no infinite loops or evaluation errors
 
 -- IO
 
-clear :: IO GHC.IO.Exception.ExitCode
-clear = system "clear"
-
-main :: IO ()
-main = welcomeMsg >> repl
+clear :: IO ()
+clear = putStr "\x1b[2J\x1b[0;0H"
 
 repl :: IO ()
 repl = do hSetBuffering stdin LineBuffering
           hSetBuffering stdout NoBuffering
           putStr "λ> "
           s <- getLine
-          case s of
+          case head $ words s of
             ":quit"   -> return ()
             ":q"      -> return ()
             ":h"      -> printHelp >> repl
@@ -170,45 +161,12 @@ repl = do hSetBuffering stdin LineBuffering
                                          repl
                            Left  e -> do print e >> repl
 
-welcomeMsg :: IO ()
-welcomeMsg = do putStrLn "Lambda Calculus Interpreter"
-                putStrLn "Version: 0.1.2"
-                putStrLn "Build Date: February 2nd, 2018"
-                putStrLn "Type :help or :h for help and information on commands"
+main :: IO ()
+main = do putStrLn "Lambda Calculus Interpreter"
+          putStrLn "Version: 0.1.3"
+          putStrLn "Build Date: February 2nd, 2018"
+          putStrLn "Type :help or :h for help and information on commands"
+          repl
 
 printHelp :: IO ()
-printHelp = do putStrLn "Lambda Calculus Interpreter"
-               putStrLn "---------------------------\n"
-               putStrLn "RULES:"
-               putStrLn " - Variable names are 1 character long\
-                          \ and can be anything except the following:"
-               putStrLn "    * Numbers\n    * Underscore (\'_\')\n    * Parens"
-               putStrLn "    * Backslash (\'\\\')\n    * Space (\' \')"
-               putStrLn " - Variable names can be surrounded by parenthesis:\
-                          \ (v) or v"
-               putStrLn " - When encoutering an ambiguous case, the interpreter\
-                        \ will try to rename \n  variables by adding a number\
-                        \ after them:"
-               putStrLn "    * (\\xx.x) is equivalent to (\\x.(\\x.x))"
-               putStrLn "     The second lambda definition shadows the first,\
-                        \ so the compiler will\n     evaluate this to \
-                        \(\\xx_1.x)"
-               putStrLn "    * (\\xy.x y) y will evaluate to (\\y_1.y y_1)"
-               putStrLn " - Functions should be preferably surrounded by\
-                        \ parenthesis: "
-               putStrLn "    * \\v.E will work, but.."
-               putStrLn "    * \\v.E x is equivalent to \\v.(E x)"
-               putStrLn "    * \\v.E x is not equivalent to (\\v.E) x"
-               putStrLn " - There can be multiple parameters for a function\
-                          \ definition:"
-               putStrLn "    * (\\x.(\\y.x)) is correct"
-               putStrLn "    * (\\xy.x) is correct and equivalent to the above"
-               putStrLn " - Application is done with spaces: (E1 E2)"
-               putStrLn " - There can be multiple arguments for the same\
-                        \ application:"
-               putStrLn "    * ((E1 v) w) is correct"
-               putStrLn "    * (E1 v w) is correct and equivalent to the above"
-               putStrLn "\nADDITIONAL COMMANDS:"
-               putStrLn " - :quit or :q will quit the interpreter."
-               putStrLn " - :help or :h will show this help message."
-               putStrLn " - :cls or :clear will clear the console."
+printHelp = readFile "help.txt" >>= putStr
